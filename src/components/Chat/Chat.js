93173dc8494a12616644f846672228c1853499da
eduviictor <eduvictornobrega@gmail.com>
2019-06-24 14:23:36 -0300

    Colocando imagem localmente e websocket separado

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,38 +1,22 @@
 import React, { Component } from 'react';
 import { StyleSheet, Alert } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat';
+import ws from '../../services/webSocket';
+import imageParrot from '../../images/papagaio_user.jpg';
 
 export default class Chat extends Component {
 
-    componentWillMount(){
-		this.setState({
-			ws: new WebSocket('wss://echo.websocket.org')
-		})
-	}
-
-	static navigationOptions = {
-		title: 'Papagaio App',
-		headerStyle: {
-			backgroundColor: '#FF890C'
-		},
-		headerTintColor: '#fff',
-			headerTitleStyle: {
-			fontWeight: 'bold',
-		},
-	};
-
 	state = {
-		ws: null,
 		messages: [],
 		message: '',
 		showAvatar: false
 	}
 
 	componentDidMount(){
-		this.state.ws.onopen = () => {
+		ws.onopen = () => {
 			console.log('Conectado!');
 		}
-		this.state.ws.onmessage = ({ data }) => {
+		ws.onmessage = ({ data }) => {
 			const parrot = {
 				_id: this.state.messages.length,
 				text: data,
@@ -40,7 +24,7 @@ export default class Chat extends Component {
 				user: {
 					_id: 2,
 					name: 'Papagaio',
-					avatar: 'https://image.freepik.com/vetores-gratis/psittacus-eximius-ilustrado_53876-34993.jpg',
+					avatar: imageParrot,
 				}
 			}
 			this.setState(previousState => ({
@@ -53,7 +37,7 @@ export default class Chat extends Component {
 		this.setState(previousState => ({
 			messages: GiftedChat.append(previousState.messages, messages),
 		}));	
-		this.state.ws.send(this.state.message);
+		ws.send(this.state.message);
 	}
 
 	showAvatar = (user) => {
---
+++ src/services/webSocket.js
@@ -0,0 +1,3 @@
+const ws = new WebSocket('wss://echo.websocket.org');
+
+export default ws;
